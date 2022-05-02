5179e2ee5e10fd208866b222afe5263205cb83f7
SwapnilPulsay9648 <88311829+SwapnilPulsay9648@users.noreply.github.com>
2022-05-02 15:05:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var ball1;
+var database;
+var position;
+function setup(){
+     database = firebase.database() 
+    createCanvas(500,500);
+    ball1 = createSprite(100,50,10,10);
+    ball1.shapeColor = "red";
+
+    var ball1position = database.ref('ball/position');
+    ball1position.on("value",readPosition,showError)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    database.ref('ball/position').set({
+      'x': position.x + x ,
+      'y': position.y + y
+    })
+  }
+ 
+function readPosition(data){
+    position = data.val();
+   
+    ball1.x = position.x;
+    ball1.y = position.y;
+  }
+  
+  function showError(){
+    console.log("Error in writing to the database");
+  }
